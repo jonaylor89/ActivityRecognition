d8b731afed66522c87d2d93aedd6a453d773fcbe
John Naylor <jonaylor89@gmail.com>
2020-01-03 11:33:24 -0500

    Initial Commit

---
+++ ActivityRecognition/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ActivityRecognition
+//
+//  Created by John Naylor on 1/3/20.
+//  Copyright © 2020 John Naylor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
